125d24fc78a9472625d0946a6591748b0f64f2c1
unclenacho <sridzik@gmail.com>
2021-10-15 21:09:08 -0400

    wake up script for mongodb atlas

---
+++ simple-crud/emergency.js
@@ -0,0 +1,15 @@
+/* 
+Example script to wake up to mongoDB Atlas connection and keep the db alive (for free)
+*/
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://simpleappadmin:Chip$$12345@simpleapp001.ltcno.mongodb.net/simpleCrud?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("simpleCrud").collection("cats");
+//   db.collection_name.find().pretty()
+  // perform actions on the collection object
+  console.log('connected');
+  client.close();
+});
+
