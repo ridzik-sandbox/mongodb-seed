7e3ae46bc7a3f736ab05ed9d7400f1dfbf8e549b
unclenacho <sridzik@gmail.com>
2021-10-15 21:14:48 -0400

    Example seeding db script that clears db documents and populates the db right back

--- simple-crud/mySeedScript.js
+++ simple-crud/mySeedScript.js
@@ -1,31 +1,51 @@
-/* mySeedScript.js */
+/* mySeedScript.js 
+Adjusted from original script provided under template/mySeedScript.js (which didn't work)
+Seeds DB on Mongo Atlas
+Uses dotenv and library that uses crypto-js to decrypt the env values for authentication
+
+If Error: MongoServerSelectionError: connection <monitor> to 54.174.213.75:27017 closed
+Refresh Whitelist IP in MongoDB Atlas
+*/
 
 // require the necessary libraries
 const faker = require("faker");
 const MongoClient = require("mongodb").MongoClient;
+const dotenv = require("dotenv").config();
+const crypto = require("./shared/crypto");
 
-function randomIntFromInterval(min, max) { // min and max included
+const randomIntFromInterval = (min, max) => { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+const {
+    MONGODB_CLUSTER,
+    MONGODB_DB,
+    MONGODB_USER,
+    MONGODB_PW,
+    } = process.env;
+
+const cluster = crypto.decrypt(MONGODB_CLUSTER);
+const db = crypto.decrypt(MONGODB_DB);
+const un = crypto.decrypt(MONGODB_USER);
+const pw = crypto.decrypt(MONGODB_PW);
+
+// Connection URL
+const uri = `mongodb+srv://${un}:${pw}@${cluster}.ltcno.mongodb.net/${db}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 async function seedDB() {
-    // Connection URL
-    const uri = "YOUR MONGODB ATLAS URI";
-
-    const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        // useUnifiedTopology: true,
-    });
-
     try {
         await client.connect();
         console.log("Connected correctly to server");
 
-        const collection = client.db("iot").collection("kitty-litter-time-series");
+        const collection = client.db("simpleCrud").collection("cats");
 
-        // The drop() command destroys all data from a collection.
+        // The deleteMany() command destroys all data from a collection. It does not remove the db or collection, just documents.
         // Make sure you run it against proper database and collection.
-        collection.drop();
+        collection.deleteMany();
 
         // make a bunch of time series data
         let timeSeriesData = [];
@@ -53,13 +73,14 @@ async function seedDB() {
             }
             timeSeriesData.push(newDay);
         }
-        collection.insertMany(timeSeriesData);
+        collection.insertMany(timeSeriesData, () => {
+            console.log("Closing DB connection");
+            client.close();
+        });
 
         console.log("Database seeded! :)");
-        client.close();
     } catch (err) {
         console.log(err.stack);
     }
 }
-
-seedDB();
+seedDB().catch(console.dir);
